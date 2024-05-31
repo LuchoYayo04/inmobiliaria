533e81fcb0e880b18165d33e26bddfe8ace5ff64
LuchoYayo4 <yayoo.hb@gmail.com>
2024-05-30 23:42:34 -0400

    Cambios listos para presentar y proxima retroalimentación de dalto.

---
+++ components/Apartment.js
@@ -0,0 +1,29 @@
+import React from "react";
+import styles from "@/styles/components/Apartment.module.css";
+
+const Apartment = ({ data }) => {
+  return (
+    <div className={styles.container}>
+      {data.map((property) => (
+        <div key={property._id} className={styles.properties__box}>
+          <h3>{property.nameApartment}</h3>
+          <p>{property.description}</p>
+          <ul>
+            <li><span>Precio:</span> ${property.price}</li>
+            <li><span>Ubicación:</span> {property.location}</li>
+            <li><span>Habitaciones:</span> {property.bedrooms}</li>
+            <li><span>Baños:</span> {property.bathrooms}</li>
+          </ul>
+          <img
+            src={`http://localhost:8080/${property.filename}`}
+            alt={property.nameApartment}
+            className={styles.property__image}
+          />
+          <a href="#">Más información</a>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Apartment;
--- pages/HomePage.js
+++ pages/HomePage.js
@@ -1,76 +1,113 @@
-import React from "react";
+import "dotenv/config";
+import React, { useState, useEffect } from "react";
 import styles from "@/styles/HomePage.module.css";
+import axios from "axios";
+import Apartment from "@/components/Apartment";
+import { FaWhatsapp, FaInstagram } from "react-icons/fa";
 
 const HomePage = () => {
+  const [data, setData] = useState([]);
+
+  const URL_BD = process.env.NEXT_PUBLIC_API_URL;
+
+  useEffect(() => {
+    const fetchProperties = async () => {
+      try {
+        const response = await axios.get(`${URL_BD}/api/apartments`);
+        setData(response.data);
+      } catch (error) {
+        console.error("Error al obtener propiedades:", error);
+      }
+    };
+
+    fetchProperties();
+  }, []);
+
+  console.log(data);
+
   return (
     <React.Fragment>
-      {/* Cabecera de la pagina */}
-      <header className={styles.header}>
-        <h1>Inmobiliaria</h1>
-        <nav className={styles.header__nav}>
-          <ul>
-            <li>
-              <a href="#inicio">Inicio</a>
-            </li>
-            <li>
-              <a href="#propiedades">Propiedades</a>
-            </li>
-            <li>
-              <a href="#contacto">Contacto</a>
-            </li>
-          </ul>
-        </nav>
-      </header>
+      <div className={styles.container}>
+        {/* Cabecera de la pagina */}
 
-      {/* Seccion de bienvenida a la pagina */}
-      <section id="inicio" className={styles.inicio}>
-        <h2>¡Bienvenido a la inmobiliaria que estabas buscando!</h2>
-        <p>
-          Somos una empresa dedicada a entregarte la mejor opción para tu
-          vivienda. Somos una empresa dedicada a entregarte la mejor opción para tu
-          vivienda. Somos una empresa dedicada a entregarte la mejor opción para tu
-          vivienda. 
-        </p>
-      </section>
+        <header className={styles.header}>
+          <h1>
+            <a href="/HomePage">Inmobiliaria</a>
+          </h1>
+          <nav className={styles.header__nav}>
+            <ul>
+              <li>
+                <a href="#inicio">Inicio</a>
+              </li>
+              <li>
+                <a href="#propiedades">Propiedades</a>
+              </li>
+              <li>
+                <a href="#contacto">Contacto</a>
+              </li>
+            </ul>
+          </nav>
+        </header>
 
-      {/* Seccion de propiedades */}
-      <section id="propiedades" className={styles.properties}>
-        <h2>Propiedades disponibles</h2>
-        {/* Lista de propiedades */}
-        <div className={styles.properties__box}>
-          <h3>Nombre de la propiedad</h3>
+        {/* Seccion de bienvenida a la pagina */}
+        <section id="inicio" className={styles.inicio}>
+          <h2>¡Bienvenido a la inmobiliaria que estabas buscando!</h2>
           <p>
-            Descripcion breve de la propiedad. Lorem ipsum dolor sit amet,
-            consectetur adipiscing elit.{" "}
+            Esto es Inmobiliaria, donde convertimos tus sueños de hogar en
+            realidad. Descubre las mejores opciones para tu vivienda con
+            nosotros, expertos en encontrar lo que realmente necesitas. Explora
+            nuestras propiedades cuidadosamente seleccionadas y déjate guiar
+            hacia el hogar perfecto para ti y tu familia.
           </p>
-          <ul>
-            <li>Precio: $XXX</li>
-            <li>Ubicacion: Ciudad, País</li>
-            <li>Habitaciones: X</li>
-            <li>Baños: X</li>
-          </ul>
-          <a href="">Más información</a>
-        </div>
-      </section>
+        </section>
 
-      {/* Sección de contacto */}
-      <section id="contacto" className={styles.section__contact}>
-        <h2>Contacto</h2>
-        <p>¿Tienes dudas? Contáctanos</p>
-        <form>
-          <label for="nombre">Nombre:</label>
-          <input type="text" id="nombre" name="nombre" required />
-          <label for="email">Correo Electrónico:</label>
-          <input type="email" id="email" name="email" required />
-          <label for="mensaje">Mensaje:</label>
-          <textarea id="mensaje" name="mensaje" required placeholder="Hola, estoy interesado en una propiedad..."/>
-          <button type="submit">Enviar</button>
-        </form>
-      </section>
+        {/* Seccion de propiedades */}
+        <section id="propiedades" className={styles.properties}>
+          <h2>Propiedades disponibles</h2>
+          {/* Lista de propiedades */}
+          <div>
+            <Apartment data={data} />
+          </div>
+        </section>
+
+        {/* Sección de contacto */}
+        <section id="contacto" className={styles.contact}>
+          <h2>Contacto</h2>
+          <p>¿Tienes dudas? Contáctanos</p>
+          <form>
+            <label for="nombre">Nombre:</label>
+            <input type="text" id="nombre" name="nombre" required />
+            <label for="email">Correo Electrónico:</label>
+            <input type="email" id="email" name="email" required />
+            <label for="mensaje">Mensaje:</label>
+            <textarea
+              id="mensaje"
+              name="mensaje"
+              required
+              placeholder="Hola, estoy interesado en una propiedad..."
+            />
+            <button type="submit">Enviar</button>
+          </form>
+        </section>
+      </div>
 
       {/* Pie de pagina */}
       <footer className={styles.footer}>
         <p>&copy; 2024 Inmobiliaria. Todos los derechos reservados.</p>
+        <div className={styles.contactInfo}>
+          <div>
+            <h4><FaWhatsapp /></h4>
+            <a href="tel:+123456789">+569 94259719</a>
+          </div>
+          <div>
+            <h4><FaInstagram/></h4>
+            <a href="#">InmobiGram</a>
+          </div>
+          <div>
+            <h4>Dirección:</h4>
+            <p>Calle, Ciudad</p>
+          </div>
+        </div>
       </footer>
     </React.Fragment>
   );
